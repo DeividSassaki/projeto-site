fd16cd54aad734ebedacd0909f68c41746a2d662
DeividSassaki <eu_sassaki@hotmail.com>
2021-06-28 01:31:27 -0300

    update

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-function button() {
 
-    document.getElementById('button01').disabled = true
-    document.getElementById('button02').disabled = false
 
-    exibereceita()
+function goBack() {
+  window.history.go(-1);
 }
-
-function exibeCartaJogador() {
-    var divCartaJogador = document.getElementById("carta-jogador")
-    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
-    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
-    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
-    var opcoesTexto = ""
-
-    for (var atributo in cartaJogador.atributos) {
-        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
-    }
-
-    var html = "<div id='opcoes' class='carta-status'>"
-
-    divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+function goForward(){
+    window.history.go(1)
+}
+function mouse(){
+    mouse.bind
 }
